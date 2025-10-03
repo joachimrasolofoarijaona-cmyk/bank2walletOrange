8e6dc36b9b554e48e5deddfc2301996633d70867
Raiba-sama <jrasolofoarijaona@gmail.com>
2025-10-03 11:01:42 +0300

    OMRequest bug fix - 03102025 - Joachim - 11:10

--- config/logging.php
+++ config/logging.php
@@ -54,9 +54,9 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
-            'ignore_exceptions' => false,
+            'channels' => ['daily', 'stderr'],
         ],
+
 
         'single' => [
             'driver' => 'single',
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
