0b64ef05e5486170d2c13a67cbe7c758096099ee
Raiba-sama <jrasolofoarijaona@gmail.com>
2025-10-10 13:21:16 +0300

    SubRequest fixing - Joachim - 10102025 - 13:12

--- resources/views/subscribeValidation.blade.php
+++ resources/views/subscribeValidation.blade.php
@@ -413,7 +413,7 @@ foreach (session('selectedRoles') as $role) {
                                 </tr>
 
                                 {{-- Cas 2 : si SOUSCRIPTION, VALIDEE, mais pas encore activée --}}
-                                @elseif($validation->request_type === 'SOUSCRIPTION' && $validation->status === "1"  && $account_subscribed === null && $hidden === "")
+                                @elseif($validation->request_type === 'SOUSCRIPTION' && $validation->status === "1"  && $account_subscribed === null)
                                 <tr>
                                     <td><strong>{{ $validation->ticket }}</strong></td>
                                     <td>{{ $validation->created_at }}</td>
