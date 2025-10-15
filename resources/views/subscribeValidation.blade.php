1162c01d5edaa10928d09851f365d881deeb11a0
Raiba-sama <jrasolofoarijaona@gmail.com>
2025-10-15 13:19:03 +0300

    validation fix - Joachim - 15102025 - 12:43

--- resources/views/subscribeValidation.blade.php
+++ resources/views/subscribeValidation.blade.php
@@ -410,11 +410,11 @@ break;
                                 </tr>
 
                                 {{-- Cas 2 : si SOUSCRIPTION, VALIDEE, mais pas encore activée et pas encore souscrit--}}
-                                @elseif(
-                                $isSouscription
-                                && $isValidated
-                                && ($account_subscribed === null || $account_subscribed->account_status === '0')
-                                && $hidden === ''
+                                    @elseif(
+                                    $isSouscription
+                                    && $isValidated
+                                    && ($account_subscribed === null || $account_subscribed->account_status === '1')
+                                    && $hidden === ''
                                 )
 
                                 <tr>
@@ -499,7 +499,7 @@ break;
                                     $isSouscription
                                     && $isValidated
                                     && $account_subscribed
-                                    && $account_subscribed->account_status === '1'
+                                    && $account_subscribed->account_status === '0'
                                     && $hidden === ''
                                 )
 
