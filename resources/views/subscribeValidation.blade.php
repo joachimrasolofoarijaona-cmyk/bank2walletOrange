0a3be18adc05b515aa858c17f6cf5bfa6de367d7
Raiba-sama <jrasolofoarijaona@gmail.com>
2025-10-15 14:41:14 +0300

    validation fix - Joachim - 15102025 - 13:50

--- resources/views/subscribeValidation.blade.php
+++ resources/views/subscribeValidation.blade.php
@@ -415,7 +415,7 @@ break;
                                         </tr>
 
                                     {{-- Cas 2 : si SOUSCRIPTION, VALIDEE, mais pas encore activée et pas encore souscrit --}}
-                                    @elseif($isSouscription && $isValidated && $not_subscribed === true && $validation->final_status === '' && $hidden === "")  
+                                    @elseif($isSouscription && $isValidated && $not_subscribed === true && $validation->final_status === null && $hidden === "")  
                                         <tr>    
                                             <td><strong>{{ $validation->ticket }}</strong></td>
                                             <td>{{ $validation->created_at }}</td>
