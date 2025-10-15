c89c080a5afe7854d2177f6f583711356bed99b8
Raiba-sama <jrasolofoarijaona@gmail.com>
2025-10-15 13:10:35 +0300

    validation fix - Joachim - 15102025 - 12:43

--- resources/views/subscribeValidation.blade.php
+++ resources/views/subscribeValidation.blade.php
@@ -490,7 +490,6 @@ break;
                                   
                                     {{-- Cas 6 : VALIDEE et SOUSCRIT --}}
                                     @elseif($isSouscription && $isValidated && $account_subscribed->account_status === '1' && $hidden === '')
-                                        @php dd($account_subscribed, $hidden); @endphp
                                         <tr>
                                             <td><strong>{{ $validation->ticket }}</strong></td>
                                             <td>{{ $validation->created_at }}</td>
