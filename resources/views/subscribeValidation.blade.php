c4c575967f8abe9315d7468ae16405e6091ea602
Raiba-sama <jrasolofoarijaona@gmail.com>
2025-10-15 14:39:52 +0300

    validation fix - Joachim - 15102025 - 13:50

--- resources/views/subscribeValidation.blade.php
+++ resources/views/subscribeValidation.blade.php
@@ -392,10 +392,10 @@ break;
                                             ->where('account_no', $validation->account_no)
                                             ->first();
                                         
-                                        $not_subscribed = true;
+                                        $not_subscribed = false;
 
                                         if($account_subscribed === null || $account_subscribed === '0'){
-                                            $not_subscribed = false;
+                                            $not_subscribed = true;
                                         }
                                     @endphp
 
@@ -415,7 +415,7 @@ break;
                                         </tr>
 
                                     {{-- Cas 2 : si SOUSCRIPTION, VALIDEE, mais pas encore activée et pas encore souscrit --}}
-                                    @elseif($isSouscription && $isValidated && $not_subscribed === 'false' && $validation->final_status === '' && $hidden === "")  
+                                    @elseif($isSouscription && $isValidated && $not_subscribed === true && $validation->final_status === '' && $hidden === "")  
                                         <tr>    
                                             <td><strong>{{ $validation->ticket }}</strong></td>
                                             <td>{{ $validation->created_at }}</td>
